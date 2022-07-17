e680692ad3c45879bcc566975587477aa60d0642
YoungJae Kim <corea_kyj@naver.com>
2022-07-17 21:37:46 +0900

    add generate api

---
+++ api/prediction.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Request
+
+from payloads.request import TextGenerationRequest
+
+
+router = APIRouter()
+
+
+@router.post("/generate")
+def post_generation(request: Request, data: TextGenerationRequest) -> str:
+    tokenizer = request.app.state.tokenizer
+    model = request.app.state.model
+    input_ids = tokenizer(data.prompt, return_tensors="pt").input_ids.cuda()
+    generated_ids = model.generate(input_ids)
+    result = tokenizer.batch_decode(generated_ids, max_new_tokens=data.max_tokens, skip_special_tokens=True)
+    return result[0]
--- payloads/request.py
+++ payloads/request.py
@@ -14,30 +14,18 @@ class TextGenerationRequest(BaseModel):
         le=2048,
         description="The maximum number of tokens to generate in the completion. The token count of your prompt plus max_tokens cannot exceed the model's context length.",
     )
-    temperature: float = Field(
-        default=1.0,
-        gt=0.0,
-        description="What sampling temperature to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer. We generally recommend altering this or `top_p` but not both.",
-    )
-    top_p: float = Field(
-        default=1.0,
-        ge=0.0,
-        le=1.0,
-        description="An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. We generally recommend altering this or `temperature` but not both.",
-    )
-    n: int = Field(
-        default=1,
-        description="How many completions to generate for each prompt.",
-    )
-    presence_penalty: float = Field(
-        default=0,
-        ge=-2.0,
-        le=2.0,
-        description="Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.",
-    )
-    frequency_penalty: float = Field(
-        default=0,
-        ge=-2.0,
-        le=2.0,
-        description="Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.",
-    )
+    # temperature: float = Field(
+    #     default=1.0,
+    #     gt=0.0,
+    #     description="What sampling temperature to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer. We generally recommend altering this or `top_p` but not both.",
+    # )
+    # top_p: float = Field(
+    #     default=1.0,
+    #     ge=0.0,
+    #     le=1.0,
+    #     description="An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. We generally recommend altering this or `temperature` but not both.",
+    # )
+    # n: int = Field(
+    #     default=1,
+    #     description="How many completions to generate for each prompt.",
+    # )
