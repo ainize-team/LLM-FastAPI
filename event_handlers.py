1d946ac8bd77352a74a9eb1b10aa2bb1202bd55f
YoungJae Kim <byeongal@comcom.ai>
2022-07-18 11:26:37 +0900

    change device_map value

--- event_handlers.py
+++ event_handlers.py
@@ -1,18 +1,33 @@
-from typing import Callable
+from typing import Callable, Dict, List
 
 import torch
 from fastapi import FastAPI
 from loguru import logger
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 
 from config import model_settings
 
 
 def _load_model(app: FastAPI) -> None:
     model_name_or_path = model_settings.model_name_or_path
+    number_of_device = torch.cuda.device_count()
+    config = AutoConfig.from_pretrained(model_name_or_path)
+    n_layer = config.n_layer
+    quotient, remainder = divmod(n_layer, number_of_device)
+    device_map: Dict[int, List[int]] = {}
+    for device_number in range(number_of_device - 1, -1, -1):
+        device_map[device_number] = []
+        for _ in range(quotient):
+            n_layer -= 1
+            device_map[device_number].append(n_layer)
+        if remainder:
+            remainder -= 1
+            n_layer -= 1
+            device_map[device_number].append(n_layer)
+    logger.info(f"Device Map : {device_map}")
     app.state.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
     app.state.model = AutoModelForCausalLM.from_pretrained(
-        model_name_or_path, device_map="auto", torch_dtype=torch.float16
+        model_name_or_path, device_map=device_map, torch_dtype=torch.bfloat16
     )
 
 
