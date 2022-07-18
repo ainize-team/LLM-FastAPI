60fb866ea835eba72f9f244715e380296d5e62c5
YoungJae Kim <byeongal@comcom.ai>
2022-07-18 11:36:30 +0900

    change device_map value (hard coding)

--- event_handlers.py
+++ event_handlers.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict, List
+from typing import Callable, Dict
 
 import torch
 from fastapi import FastAPI
@@ -14,16 +14,92 @@ def _load_model(app: FastAPI) -> None:
     config = AutoConfig.from_pretrained(model_name_or_path)
     n_layer = config.n_layer
     quotient, remainder = divmod(n_layer, number_of_device)
-    device_map: Dict[int, List[int]] = {}
-    for device_number in range(number_of_device - 1, -1, -1):
-        device_map[device_number] = []
+    # TODO: generalize
+    layer_list = [
+        "transformer.word_embeddings",
+        "lm_head",
+        "transformer.word_embeddings_layernorm",
+        "transformer.h.0",
+        "transformer.h.1",
+        "transformer.h.2",
+        "transformer.h.3",
+        "transformer.h.4",
+        "transformer.h.5",
+        "transformer.h.6",
+        "transformer.h.7",
+        "transformer.h.8",
+        "transformer.h.9",
+        "transformer.h.10",
+        "transformer.h.11",
+        "transformer.h.12",
+        "transformer.h.13",
+        "transformer.h.14",
+        "transformer.h.15",
+        "transformer.h.16",
+        "transformer.h.17",
+        "transformer.h.18",
+        "transformer.h.19",
+        "transformer.h.20",
+        "transformer.h.21",
+        "transformer.h.22",
+        "transformer.h.23",
+        "transformer.h.24",
+        "transformer.h.25",
+        "transformer.h.26",
+        "transformer.h.27",
+        "transformer.h.28",
+        "transformer.h.29",
+        "transformer.h.30",
+        "transformer.h.31",
+        "transformer.h.32",
+        "transformer.h.33",
+        "transformer.h.34",
+        "transformer.h.35",
+        "transformer.h.36",
+        "transformer.h.37",
+        "transformer.h.38",
+        "transformer.h.39",
+        "transformer.h.40",
+        "transformer.h.41",
+        "transformer.h.42",
+        "transformer.h.43",
+        "transformer.h.44",
+        "transformer.h.45",
+        "transformer.h.46",
+        "transformer.h.47",
+        "transformer.h.48",
+        "transformer.h.49",
+        "transformer.h.50",
+        "transformer.h.51",
+        "transformer.h.52",
+        "transformer.h.53",
+        "transformer.h.54",
+        "transformer.h.55",
+        "transformer.h.56",
+        "transformer.h.57",
+        "transformer.h.58",
+        "transformer.h.59",
+        "transformer.h.60",
+        "transformer.h.61",
+        "transformer.h.62",
+        "transformer.h.63",
+        "transformer.h.64",
+        "transformer.h.65",
+        "transformer.h.66",
+        "transformer.h.67",
+        "transformer.h.68",
+        "transformer.h.69",
+        "transformer.ln_f",
+    ]
+    device_map: Dict[str, int] = {}
+    idx = 0
+    for i in range(number_of_device):
         for _ in range(quotient):
-            n_layer -= 1
-            device_map[device_number].append(n_layer)
-        if remainder:
-            remainder -= 1
-            n_layer -= 1
-            device_map[device_number].append(n_layer)
+            device_map[layer_list[idx]] = i
+            idx += 1
+        if i >= number_of_device - remainder:
+            device_map[layer_list[idx]] = i
+            idx += 1
     logger.info(f"Device Map : {device_map}")
     app.state.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
     app.state.model = AutoModelForCausalLM.from_pretrained(
