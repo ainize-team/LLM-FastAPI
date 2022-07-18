5df2f10c37f82761b4732521566413474e38c85a
YoungJae Kim <byeongal@comcom.ai>
2022-07-18 12:06:37 +0900

    add request parmas

--- api/prediction.py
+++ api/prediction.py
@@ -11,6 +11,14 @@ def post_generation(request: Request, data: TextGenerationRequest) -> str:
     tokenizer = request.app.state.tokenizer
     model = request.app.state.model
     input_ids = tokenizer(data.prompt, return_tensors="pt").input_ids.cuda()
-    generated_ids = model.generate(input_ids, max_new_tokens=data.max_tokens)
-    result = tokenizer.batch_decode(generated_ids, max_new_tokens=data.max_tokens, skip_special_tokens=True)
+    generated_ids = model.generate(
+        input_ids,
+        max_new_tokens=data.max_tokens,
+        temperature=data.temperature,
+        top_k=data.top_k,
+        top_p=data.top_p,
+        repetition_penalty=data.repetition_penalty,
+        do_sample=data.do_sample,
+    )
+    result = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
     return result[0]
--- payloads/request.py
+++ payloads/request.py
@@ -14,18 +14,23 @@ class TextGenerationRequest(BaseModel):
         le=2048,
         description="The maximum number of tokens to generate in the completion. The token count of your prompt plus max_tokens cannot exceed the model's context length.",
     )
-    # temperature: float = Field(
-    #     default=1.0,
-    #     gt=0.0,
-    #     description="What sampling temperature to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer. We generally recommend altering this or `top_p` but not both.",
-    # )
-    # top_p: float = Field(
-    #     default=1.0,
-    #     ge=0.0,
-    #     le=1.0,
-    #     description="An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. We generally recommend altering this or `temperature` but not both.",
-    # )
-    # n: int = Field(
-    #     default=1,
-    #     description="How many completions to generate for each prompt.",
-    # )
+    temperature: float = Field(
+        default=0.9,
+        gt=0.0,
+        description="What sampling temperature to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer. We generally recommend altering this or `top_p` but not both.",
+    )
+    top_k: int = Field(
+        default=50, description="The number of highest probability vocabulary tokens to keep for top-k-filtering."
+    )
+    top_p: float = Field(
+        default=0.95,
+        ge=0.0,
+        le=1.0,
+        description="An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. We generally recommend altering this or `temperature` but not both.",
+    )
+    repetition_penalty: float = Field(
+        default=0.8, gt=0.0, description="The parameter for repetition penalty. 1.0 means no penalty."
+    )
+    do_sample: bool = Field(
+        default=True, description="Whether or not to use sampling ; use greedy decoding otherwise."
+    )
