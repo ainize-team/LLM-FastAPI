04557686ecb1ab5e5c342258a00172a26fc9ce4f
YoungJae Kim <byeongal@comcom.ai>
2022-07-18 11:52:54 +0900

    use max_new_tokens when generate text

--- api/prediction.py
+++ api/prediction.py
@@ -11,6 +11,6 @@ def post_generation(request: Request, data: TextGenerationRequest) -> str:
     tokenizer = request.app.state.tokenizer
     model = request.app.state.model
     input_ids = tokenizer(data.prompt, return_tensors="pt").input_ids.cuda()
-    generated_ids = model.generate(input_ids)
+    generated_ids = model.generate(input_ids, max_new_tokens=data.max_tokens)
     result = tokenizer.batch_decode(generated_ids, max_new_tokens=data.max_tokens, skip_special_tokens=True)
     return result[0]
