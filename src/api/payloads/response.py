981fac8dfb332f4008580a09338d745d30ebb971
godhw <shw0210@naver.com>
2022-07-27 19:04:01 +0900

    add response type for get api

--- src/api/payloads/response.py
+++ src/api/payloads/response.py
@@ -1,5 +1,18 @@
-from pydantic import BaseModel
+from typing import List
+
+from pydantic import BaseModel, Field
 
 
 class AsyncTaskResponse(BaseModel):
     task_id: str
+
+
+class TextGenerationResponse(BaseModel):
+    status: str = Field(
+        default="wait",
+        description="Check the result is generated. wait means the result is not ready yet.",
+    )
+    result: List[str] = Field(
+        default=[""],
+        description="If status is ok, it has the result value. Otherwise, default value.",
+    )
