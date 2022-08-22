f03c3bd5a629b43893f9906520339c0f94f5e58f
YoungJae Kim <byeongal@comcom.ai>
2022-08-22 13:30:05 +0900

    fix venv path

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ if [[ ! -v NUMBER_OF_WORKERS ]]; then
     export NUMBER_OF_WORKERS=$(grep -c processor /proc/cpuinfo)
 fi
 
-. /application_root/.venv/bin/activate
+. /app/.venv/bin/activate
 
 gunicorn --workers ${NUMBER_OF_WORKERS} --bind 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker server:app
